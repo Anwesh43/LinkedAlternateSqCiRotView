dd1696fcc39442a626238e60b308bf23ad085169
Anwesh43 <anweshthecool0@gmail.com>
2019-06-09 13:01:12 +0530

    adding AlternateSqCiRot class

--- alternatesqcirotview/src/main/java/com/anwesh/uiprojects/alternatesqcirotview/AlternateSqCiRotView.kt
+++ alternatesqcirotview/src/main/java/com/anwesh/uiprojects/alternatesqcirotview/AlternateSqCiRotView.kt
@@ -186,7 +186,30 @@ class AlternateSqCiRotView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class AlternateSqCirRot(var i : Int) {
+
+        private val root : ASCNode = ASCNode(0)
+        private var curr : ASCNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
