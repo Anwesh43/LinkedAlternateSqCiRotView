c1f9ded7669183c8de47c79a77c46e483328e796
Anwesh43 <anweshthecool0@gmail.com>
2019-06-09 12:58:57 +0530

    adding ASCNode class

--- alternatesqcirotview/src/main/java/com/anwesh/uiprojects/alternatesqcirotview/AlternateSqCiRotView.kt
+++ alternatesqcirotview/src/main/java/com/anwesh/uiprojects/alternatesqcirotview/AlternateSqCiRotView.kt
@@ -141,8 +141,52 @@ class AlternateSqCiRotView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class ASCNode(var i : Int, val state : State = State()) {
+
+        private var prev : ASCNode? = null
+        private var next : ASCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ASCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawASCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ASCNode {
+            var curr : ASCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
         }
     }
 }
