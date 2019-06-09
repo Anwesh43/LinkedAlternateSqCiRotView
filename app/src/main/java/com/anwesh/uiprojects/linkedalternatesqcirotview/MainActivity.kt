22894a2ff4634f5d30d1f93a44ad9b4f644ed1ce
Anwesh43 <anweshthecool0@gmail.com>
2019-06-09 13:18:48 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedalternatesqcirotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedalternatesqcirotview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedalternatesqcirotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.alternatesqcirotview.AlternateSqCiRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AlternateSqCiRotView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
